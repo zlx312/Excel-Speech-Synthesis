4440af4096adbefaf420962612f20c1e672e00aa
360BUYAD\zhaolinxue <zhaolinxue@jd.com>
2018-07-19 18:11:46 +0800

    初步文本转语音

---
+++ speech/src/main/java/com/zlx312/speech/common/DemoException.java
@@ -0,0 +1,10 @@
+package com.zlx312.speech.common;
+
+/**
+ * DEMO自带的Exception
+ */
+public class DemoException extends Exception {
+    public DemoException(String message) {
+        super(message);
+    }
+}
